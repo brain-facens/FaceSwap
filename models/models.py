3b04285587ee372efc4970e83b89c9277d1d9681
Matheus Oliveira de Souza <msouza.os@hotmail.com>
2024-04-15 10:11:15 -0300

    delete: removing unnecessary files and folders.

--- models/models.py
+++
@@ -1,6 +0,0 @@
-def create_model(opt):
-    #from .pix2pixHD_model import Pix2PixHDModel, InferenceModel
-    from .fs_model import fsModel
-    model = fsModel()
-    model.initialize(opt)
-    return model
